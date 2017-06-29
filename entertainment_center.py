9486462c56386471608b6893d9b0d98d844e8c85
Maueny <derickhsm@gmail.com>
2017-06-29 00:27:50 -0700

    PEP8 Check
    PEP8 Check

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,7 @@
-import fresh_tomatoes # fresh_tomatoes.py
-import media # media.py
-
-
+import fresh_tomatoes
+#  fresh_tomatoes.py
+import media
+#   media.py
 
 # movie 1(John wick2)
 
@@ -11,7 +11,7 @@ John_wick2 = media.Movie('John wick2',
                           been put on his life',
                          'https://goo.gl/yvz57J',
                          'https://youtu.be/XGk2EfbD_Ps')
-                         # Related info(Name/Storyline/Poster/trailer)
+# Related info(Name/Storyline/Poster/trailer)
 
 # movie 2(Kingsman)
 
@@ -19,7 +19,7 @@ Kingsman = media.Movie('Kingsman',
                        'Kingsman: The Secret Service Official Trailer#1(2015)',
                        'https://goo.gl/v5ffpG',
                        'https://www.youtube.com/watch?v=hN0JkFrvO_M')
-                       # Related info(Name/Storyline/Poster/trailer)
+# Related info(Name/Storyline/Poster/trailer)
 
 # movie 3(Mad Max: Fury Road)
 
@@ -31,8 +31,9 @@ Mad_Max_Fury_Road = media.Movie('Mad Max: Fury Road',
                                  drifter named Max',
                                 'https://goo.gl/JZvQqh',
                                 'https://www.youtube.com/watch?v=hEJnMQG9ev8')
-                                 # Related info(Name/Storyline/Poster/trailer)
+# Related info(Name/Storyline/Poster/trailer)
 
-movies = [John_wick2,Kingsman,Mad_Max_Fury_Road]
-fresh_tomatoes.open_movies_page(movies) ## open the output file in the browser
+movies = [John_wick2, Kingsman, Mad_Max_Fury_Road]
+fresh_tomatoes.open_movies_page(movies)
 
+# open the output file in the browser
